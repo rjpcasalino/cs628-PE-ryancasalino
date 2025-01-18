dc679177b8ad81d8eeb3d4004453c2d41067c888
rjpc <ryan@rjpc.net>
2025-01-18 00:39:34 -0800

    add react examples

--- TP/my-next-app/app/page.tsx
+++ TP/my-next-app/app/page.tsx
@@ -1,29 +1,126 @@
-function Item({ name, isPacked }) {
+'use client'
+import { useState } from 'react';
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      You clicked me {count} times
+    </button>
+  );
+}
+
+function Item({ name, isDone }) {
   return (
     <li className="item">
-      {name} {isPacked ? '✅' : '❌'}
+      {name} {isDone ? '✅' : '❌'}
     </li>
   );
 }
 
-export default function PackingList() {
+
+ function MySearch() {
+  function publish(formData) {
+    const content = formData.get("content");
+    const button = formData.get("button");
+    alert(`'${content}' was published with the '${button}' button`);
+  }
+
+  function save(formData) {
+    const content = formData.get("content");
+    alert(`Your draft of '${content}' has been saved!`);
+  }
+
   return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item 
-          isPacked={true} 
-          name="Space suit" 
-        />
-        <Item 
-          isPacked={true} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
+    <form action={publish}>
+      <textarea name="content" rows={4} cols={40} />
+      <br />
+      <button type="submit" name="button" value="submit">Publish</button>
+      <button formAction={save}>Save draft</button>
+    </form>
   );
 }
+
+function MyForm() {
+  const [firstName, setFirstName] = useState('');
+  const [age, setAge] = useState('20');
+  const ageAsNumber = Number(age);
+  return (
+    <>
+      <label>
+        First name:
+        <input
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Age:
+        <input
+          value={age}
+          onChange={e => setAge(e.target.value)}
+          type="number"
+        />
+        <button onClick={() => setAge(ageAsNumber + 10)}>
+          Add 10 years
+        </button>
+      </label>
+      {firstName !== '' &&
+        <p>Your name is {firstName}.</p>
+      }
+      {ageAsNumber > 0 &&
+        <p>Your age is {ageAsNumber}.</p>
+      }
+    </>
+  );
+}
+
+function Blog() {
+  return (
+    <>
+      <Post title="An update" body="It's been a while since I posted..." />
+      <Post title="My new blog" body="I am starting a new blog!" />
+    </>
+  )
+}
+
+function Post({ title, body }) {
+  return (
+    <>
+      <PostTitle title={title} />
+      <PostBody body={body} />
+    </>
+  );
+}
+
+function PostTitle({ title }) {
+  return <h1>{title}</h1>
+}
+
+function PostBody({ body }) {
+  return (
+    <article>
+      <p>{body}</p>
+    </article>
+  );
+}
+
+
+
+
+export default function ToDoList() {
+  return (
+    // only one parent allowed in jsx
+    // <Item name="Nope" isDone="false"/> // example of bad way to do what's done below 
+    <div>
+    <Blog/>
+    <Item isDone={true} name="Get Dockerfile sorta working with local dev" />
+    <Item isDone={false} name="Own a home"/>
+    <MyForm />
+    <MySearch/>
+    <Counter/>
+    </div>
+  );
+}
+
